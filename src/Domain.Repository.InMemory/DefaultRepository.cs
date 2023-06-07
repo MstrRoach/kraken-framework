e69c732ce28568bab998a82bbb6c6df0bf3a1eeb
Jesus Antonio Martinez <imct.jesus.antonio@gmail.com>
2023-06-06 21:10:24 -0600

    Agregando componente para los repositorios en memoria

---
+++ src/Domain.Repository.InMemory/DefaultRepository.cs
@@ -0,0 +1,111 @@
+﻿using Kraken.Domain.Core;
+using Kraken.Domain.Storage;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repository.InMemory;
+
+/// <summary>
+/// Repositorio por defecto para realizar en memoria
+/// </summary>
+/// <typeparam name="TAggregate"></typeparam>
+/// <typeparam name="TType"></typeparam>
+/// <typeparam name="TId"></typeparam>
+public class DefaultRepository<TAggregate, TType, TId> : IRepository<TAggregate>
+    where TAggregate : Aggregate<TType, TId>, IAggregate
+    where TId : IComparable
+{
+    private static ConcurrentDictionary<TId, TAggregate> Aggregates = new();
+    
+    /// <summary>
+    /// Agrega un nuevo registro del aggregado a la lista
+    /// </summary>
+    /// <param name="aggregate"></param>
+    /// <returns></returns>
+    public Task Create(TAggregate aggregate)
+    {
+        Aggregates[aggregate.Id] = aggregate;
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Elimina una instancia del agregado
+    /// </summary>
+    /// <param name="aggregate"></param>
+    /// <returns></returns>
+    public Task Delete(TAggregate aggregate)
+    {
+        Aggregates.TryRemove(aggregate.Id, out _);
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Obtiene un agregado a partir de una especificacion de
+    /// filtro
+    /// </summary>
+    /// <param name="specification"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Task<TAggregate> Get(ISpecification<TAggregate> specification)
+    {
+        var aggregate = Aggregates
+            .Values
+            .Where(specification.IsSatisfied)
+            .FirstOrDefault();
+        return Task.FromResult(aggregate);
+    }
+
+    /// <summary>
+    /// Obtiene una lista de registros que coinciden con la especificacion
+    /// de filtro
+    /// </summary>
+    /// <param name="specification"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Task<List<TAggregate>> GetAll(ISpecification<TAggregate> specification)
+    {
+        var aggregates = Aggregates
+           .Values
+           .Where(specification.IsSatisfied)
+           .ToList();
+        return Task.FromResult(aggregates);
+    }
+
+    /// <summary>
+    /// Actualiza una entidad
+    /// </summary>
+    /// <param name="aggregate"></param>
+    /// <returns></returns>
+    public Task Update(TAggregate aggregate)
+    {
+        Aggregates[aggregate.Id] = aggregate;
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Verifica la existencia para un filtro especificado
+    /// </summary>
+    /// <param name="specification"></param>
+    /// <returns></returns>
+    public Task<bool> Exist(ISpecification<TAggregate> specification)
+            => Task.FromResult(Aggregates.Values.Any(specification.IsSatisfied));
+
+    /// <summary>
+    /// Cuenta la cantidad de elementos que coinciden con la busqueda
+    /// </summary>
+    /// <param name="specification"></param>
+    /// <returns></returns>
+    public Task<int> Count(ISpecification<TAggregate>? specification = null)
+    {
+        var quantity = specification is not null ?
+            Aggregates.Values.Count(specification.IsSatisfied) :
+            Aggregates.Values.Count;
+        return Task.FromResult(quantity);
+    }
+}
---
+++ src/Domain.Repository.InMemory/RepositoryExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Kraken.Domain;
+using Kraken.Domain.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Domain.Repository.InMemory;
+
+public static class DomainDrivenDesignOptionsExtensions
+{
+    public static DomainDrivenDesignOptions UseInMemoryRepository(this DomainDrivenDesignOptions extension)
+    {
+        extension.RegisterRepository(new RepositoryExtensions());
+        return extension;
+    }
+}
+
+/// <summary>
+/// Configuracion para el registro de los repositorios
+/// </summary>
+public class RepositoryExtensions : IRepositoryExtension
+{
+    public void AddServices(IServiceCollection services, List<Type> Aggregates)
+    {
+        var repositoryOpenType = typeof(IRepository<>);
+        var implementationRepositoryOpenType = typeof(DefaultRepository<,,>);
+        // Recorremos cada tipo de agregado
+        foreach (var aggregate in Aggregates)
+        {
+            // Obtenemos el tipo, el id
+            var baseType = aggregate.BaseType;
+            var repositoryType = repositoryOpenType.MakeGenericType(aggregate);
+            var implementationRepositoryType = implementationRepositoryOpenType.MakeGenericType(
+                aggregate, 
+                baseType.GenericTypeArguments[0], 
+                baseType.GenericTypeArguments[1]);
+            // Registramos el repositorio
+            services.AddScoped(repositoryType, implementationRepositoryType);
+        }
+    }
+}
