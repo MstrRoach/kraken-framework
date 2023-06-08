22029ec7670620cddab38bfb57577b30779dfbba
Jesus Antonio Martinez <imct.jesus.antonio@gmail.com>
2023-06-08 16:45:46 -0600

    feat: Modificaciones para ejemplo

--- src/Clients/KrakenThreeApi/Controllers/WeatherForecastController.cs
+++ src/Clients/KrakenThreeApi/Controllers/WeatherForecastController.cs
@@ -2,36 +2,49 @@ using AccessControl.App.Test;
 using Kraken.Server.Request;
 using Microsoft.AspNetCore.Mvc;
 
-namespace KrakenThreeApi.Controllers
+namespace KrakenThreeApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-        private readonly ILogger<WeatherForecastController> _logger;
-        private readonly IAppHost _appHost;
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IAppHost appHost)
-        {
-            _logger = logger;
-            _appHost = appHost;
-        }
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IAppHost _appHost;
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IAppHost appHost)
+    {
+        _logger = logger;
+        _appHost = appHost;
+    }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public async Task<IEnumerable<WeatherForecast>> Get()
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        await _appHost.ExecuteAsync(new SayHelloWorldCommand());
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            await _appHost.ExecuteAsync(new SayHelloWorldCommand());
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpPost("Account")]
+    public async Task<IActionResult> CreateAccountAsync()
+    {
+        var response = await _appHost.ExecuteAsync(new SayHelloWorldCommand());
+        return StatusCode(StatusCodes.Status201Created, response);
+    }
+
+    [HttpGet("Account")]
+    public async Task<IActionResult> GetAllAccount()
+    {
+        var response = await _appHost.ReadAsync(new GetAllAccounts());
+        return StatusCode(StatusCodes.Status200OK, response);
     }
 }
