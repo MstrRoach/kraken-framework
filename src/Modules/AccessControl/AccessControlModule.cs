cbe8064fb9bda56aa60d8f4a5f24115000cf3204
Jesus Antonio Martinez <imct.jesus.antonio@gmail.com>
2023-06-18 09:29:40 -0600

    feat: Actualizacion de proyecto de prueba

--- src/Modules/AccessControl/AccessControlModule.cs
+++ src/Modules/AccessControl/AccessControlModule.cs
@@ -1,5 +1,4 @@
-﻿using Dottex.Domain.AuditStorage.InMemory;
-using Dottex.Domain.Repository.InMemory;
+﻿using Dottex.Domain.Repository.InMemory;
 using Dottex.Kalypso.Domain;
 using Dottex.Kalypso.Module.Common;
 using Dottex.Kalypso.Module.Transaction;
@@ -17,7 +16,6 @@ public class AccessControlModule : IModule
         services.AddDomainDrivenDesign<AccessControlModule>(builder =>
         {
             builder.UseInMemoryRepository();
-            builder.UseInMemoryAuditStorage();
         });
     }
 }
