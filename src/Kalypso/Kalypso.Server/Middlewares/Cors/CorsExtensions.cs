4c9e123853b3e861faf13af069bf992dd57caf3f
Jesus Antonio Martinez <imct.jesus.antonio@gmail.com>
2023-11-24 10:47:54 -0600

    feat: Actualizacion de las referencias

--- src/Kalypso/Kalypso.Server/Middlewares/Cors/CorsExtensions.cs
+++ src/Kalypso/Kalypso.Server/Middlewares/Cors/CorsExtensions.cs
@@ -65,30 +65,4 @@ public static class CorsExtensions
         app.UseCors(options.Name);
         return app;
     }
-
-    /// <summary>
-    /// Agrega la politica de cors al descriptor de la aplicacion
-    /// </summary>
-    /// <param name="Dottex.KalypsoOptions"></param>
-    /// <param name="setup"></param>
-    /// <returns></returns>
-    /// <exception cref="InvalidOperationException"></exception>
-    /// <exception cref="ArgumentNullException"></exception>
-    public static ServerDescriptor AddCorsPolicy(this ServerDescriptor server,
-        Action<CorsOptions> setup)
-    {
-        if (server.Cors is not null)
-            throw new InvalidOperationException("Cors already been configured. See your kalypso host builder");
-        // Si las opciones de cors son nulas lanzamos excepcion
-        if (setup is null)
-            throw new ArgumentNullException(nameof(setup));
-        // Creamos las configuraciones por defecto
-        var options = new CorsOptions();
-        // Ejecutamos las configuraciones
-        setup(options);
-        // Setteammos la configuracion en las opciones de Dottex.Kalypso
-        server.Cors = new CorsFeature(options);
-        // Devolvemos las opciones de Dottex.Kalypso
-        return server;
-    }
 }
